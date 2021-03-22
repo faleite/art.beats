7c2ebbf1be7060baeda871606c7756dcbf41fe8f
faleite <fabricio_2310@hotmail.com>
2021-03-22 21:14:15 +0000

    Extraida a configuração do Secret Key
    Close#28

--- beats/settings.py
+++ beats/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fq5p2som5w(adonmzhi-_1as6kn!o+&ssqx++d!i6(z_bz0qi^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
