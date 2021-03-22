e2622f9dd090d792def7e982baf7650dcff172e6
faleite <fabricio_2310@hotmail.com>
2021-03-22 20:39:23 +0000

    Instalado python-decouple, setado configurações de instância
    Close#26

--- beats/settings.py
+++ beats/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fq5p2som5w(adonmzhi-_1as6kn!o+&ssqx++d!i6(z_bz0qi^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
