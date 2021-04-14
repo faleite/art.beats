75e8c04fed5f7481986dca9319dbbfb9d4d514ce
faleite <fabricio_2310@hotmail.com>
2021-04-14 18:35:49 +0100

    Feito lista de sons no tamplate songs
    close #115

--- beats/sounds/tests.py
+++ beats/sounds/tests.py
@@ -6,16 +6,24 @@ from beats.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
-    return client.get(reverse('sounds:sounds', args=('beat-it',)))
+    return client.get(reverse('sounds:sounds'))
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_title_sound(resp):
-    assert_contains(resp, '<h2>Beat it</h2>')
+@pytest.mark.parametrize(
+    'title',
+    [
+        'Beat it',
+        'Isn’t she lovely',
+    ]
+)
+def test_title_sound(resp, title):
+    assert_contains(resp, title)
 
 
 def test_content_sound(resp):
     assert_contains(resp, 'src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/412269699')
+    assert_contains(resp, 'src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/410849205')
